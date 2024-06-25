ac3afe942f3d7aebf453e345ca6d8afd80475315
JFinare <mate.kemenczey@gmail.com>
2024-06-25 10:02:25 +0200

    coding done except duplicate stuff

--- coding/index.js
+++ coding/index.js
@@ -13,9 +13,44 @@ Each object should have:
 value: the common element.
 indexSum: the sum of the indices of this element in both arrays.
 */
-export function twistedArrayIntersection(arr1, arr2) {}
+export function twistedArrayIntersection(arr1, arr2) {
+  const elementOne = {};
+  arr1.forEach((element, index) => {
+    if (!elementOne[element]) {
+      elementOne[element] = [];
+    }
+    elementOne[element].push(index);
+  });
 
+  const elementTwo = {};
+  arr2.forEach((element, index) => {
+    if (!elementTwo[element]) {
+      elementTwo[element] = [];
+    }
+    elementTwo[element].push(index);
+  });
 
+  const commonElements = Object.keys(elementOne).filter((element) =>
+    elementTwo.hasOwnProperty(element)
+  );
+
+  const result = [];
+  commonElements.forEach((element) => {
+    const indexOne = elementOne[element];
+    const indexTwo = elementTwo[element];
+
+    indexOne.forEach((index1) => {
+      indexTwo.forEach((index2) => {
+        result.push({
+          value: parseInt(element),
+          indexSum: index1 + index2,
+        });
+      });
+    });
+  });
+
+  return result;
+}
 /*
 You need to create an advanced accounting system that can handle basic transactions
 with some added validation. The system should process a list of transactions,
@@ -32,4 +67,22 @@ The initial balance is 0.
 If a withdrawal causes the balance to go below zero, the transaction should be ignored,
 and the function should continue processing the remaining transactions.
 */
-export function processTransactions(transactions) {}
+export function processTransactions(transactions) {
+  let balance = 0;
+
+  transactions.forEach((transaction) => {
+    const { type, amount } = transaction;
+
+    if (type === "deposit") {
+      balance += amount;
+    } else if (type === "withdrawal") {
+      if (balance >= amount) {
+        balance -= amount;
+      }
+    } else {
+      console.log("error with type: ", type);
+    }
+  });
+
+  return balance;
+}
